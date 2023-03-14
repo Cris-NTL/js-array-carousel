6482bd4ded9b853e91d9c0b4351eda7c0a9e0099
Cris-NTL <natolicristian92@gmail.com>
2023-03-14 19:09:06 +0100

    Add: js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,50 @@
 // array whit images
-const imagesArray = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg']
+const imagesArray = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 // container html
-const imagesContainer = document.querySelector('.slider-items')
-// cicle for (display array on html)
+const imagesContainer = document.querySelector('.slider-items');
+// loop to display array on html
 for (let i = 0; i < imagesArray.length; i++) {
-    const currentImage = imagesArray[i]
+    const currentImage = imagesArray[i];
     const sliderItems = `<div class="item"><img src="${currentImage}" alt="image"></div>`;
     imagesContainer.innerHTML += sliderItems;
 }
+
+// add "active" class to the first element
+const itemsArray = document.getElementsByClassName('item');
+
+let activeItem = 0;
+itemsArray[activeItem].classList.add('active');
+
+// buttons management
+const upBtn = document.querySelector(".up-btn");
+const downBtn = document.querySelector(".down-btn");
+
+// hide the up button
+upBtn.style.display = "none";
+
+downBtn.addEventListener('click', function () {
+    upBtn.style.display = "block";
+    if (activeItem < (imagesArray.length - 1)) {
+        itemsArray[activeItem].classList.remove("active");
+        activeItem++;
+        itemsArray[activeItem].classList.add("active");
+    }
+    // hide the down button if it reaches the last item
+    if (activeItem === imagesArray.length - 1) {
+        downBtn.style.display = "none";
+    }
+});
+
+upBtn.addEventListener('click', function () {
+    downBtn.style.display = "block";
+    if (activeItem > 0) {
+        itemsArray[activeItem].classList.remove('active');
+        activeItem--;
+        itemsArray[activeItem].classList.add('active');
+    }
+    // hide the up button if it reaches the first item
+    if (activeItem === 0) {
+        upBtn.style.display = "none";
+    }
+})
 
